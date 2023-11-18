2c2cf1ee6a6813836bcc6fb2c8989d1ad480b925
jeanroths <jean.machado@sou.inteli.edu.br>
2023-11-18 00:22:44 -0300

    prova1

--- chatbot_P1/chatbot.py
+++ chatbot_P1/chatbot.py
@@ -2,7 +2,7 @@
 import re
 
 def respostas(ask):
-    if ask == " crédito ?":
+    if ask == " crédito?":
         print(f'não, faz o L')
     else:
         print('faz o L')
@@ -32,27 +32,27 @@ def chatbot_prompt():
     while True:
         comando = input("Olá, como posso te mandar fazer o L hoje?: ").lower().strip()
         action = chatbot_action(comando)
-        if action == "Atualizar_pagamento" | "Acompanhar_pedido":
+        if action == "Atualizar_pagamento" or action == "Acompanhar_pedido":
             chat_responses(action, comando)
         else:
-            chat_responses(action, None)
+            chat_responses(action, comando)
 
 def chat_responses(intent, command):
     if intent:
-        if intent == 'Atualizar_pagamento' | 'Acompanhar_pedido':
+        if intent in ['Atualizar_pagamento', 'Acompanhar_pedido']:
             for key, value in intent_dict.items():
                 pattern = re.compile(key)
                 groups = pattern.findall(command)
                 if groups:
-                    print(f'{action_dict[value](groups[0])} ', end = "")
+                    response = action_dict[value](groups[0])
+                    if response:
+                        print(response, end="")
                     return  # Sai da função após o movimento
-
         elif intent == 'turn_off':
             print('Até logo')
             exit()
-
     else:
-        print('Desculpa ai, não entendi oq vc disse, faz o L')
+        print('Desculpa ai, não entendi o que você disse, faz o L')
 
 
 def chatbot_action(command):
